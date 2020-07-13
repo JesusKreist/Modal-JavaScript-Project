e15837491c0195220b75b7434e049ef3b9aed078
Ude Samuel <59575776+JesusKreist@users.noreply.github.com>
2020-07-13 13:53:18 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,78 +1,164 @@
-//Use an IIFE to avoid contanminating global namespce
-(function(){
-  //Grab stores items from the DOM
-  let storeItems = document.querySelectorAll('.store-item');
-  //grab lightbox container 
-  let lightBox = document.querySelector('.lightbox-container');
-   //grab the div with the lightbox item
-  let lightBoxItem = document.querySelector('.lightbox-item');
-  //grab all the images from the store items
-  let images = document.querySelectorAll('.store-img');
-  
-  
-  // set up an array for the items
-  let imageList = [];
-  //set up a counter to run through the list of images
-  let imageCounter = 0;
-  //use a forEach loop to get a copy of all the images and push into an array of items
-  images.forEach(function(image){
-    //push each imageto the array of images
-    imageList.push(image.src);
-  })
-  
-  //Add an a click event listener to each store item
-  storeItems.forEach(function(item) {
-    //On click, allow the model container to show 
-    //Change css class from display none to display block
-    item.addEventListener('click', function(e){
-    //grab the image of the item that was clicked
-    let image = e.target.src;
-    //change the background img property of the lightbox item
-    lightBoxItem.style.backgroundImage = `url(${image})`;
-    //show the modal with the selected image
-    lightBox.classList.add('show');
-    //get the array index number for the image that was selected
-    imageCounter = imageList.indexOf(image);
-    });
-  }); 
-  
-  //wire up the left and right buttons
-  //select left button from the DOM
-  let btnLeft = document.querySelector('.btnLeft');
-  btnLeft.addEventListener('click', function(){
-    imageCounter--;
-    if (imageCounter < 0){
-      imageCounter = imageList.length -1;
+// filterButton start
+
+(function () {
+  // vscode commit test
+
+  const el = document.getElementsByClassName("store-item");
+  const inputBar = document.getElementById("search-item");
+  const filterButtons = document.getElementsByClassName("filter-btn");
+
+  const setBlock = (event) => {
+    if (event) {
+      event.preventDefault();
     }
-    lightBoxItem.style.backgroundImage = `url(${imageList[imageCounter]})`
-  });
-    //select left button from the DOM
-  let btnRight = document.querySelector('.btnRight');
-  btnRight.addEventListener('click', function(){
-    imageCounter++;
-    if (imageCounter >= imageList.length){
-      imageCounter = 0;
+    for (let i = 0; i < 12; i++) {
+      el[i].style.display = "block";
     }
-    lightBoxItem.style.backgroundImage = `url(${imageList[imageCounter]})`;
-  });
-  
-  //wire up the modal's close button
-    let lightBoxClose = document.querySelector('.lightbox-close');
-    lightBoxClose.addEventListener('click', function(){
-    lightBox.classList.remove('show');
-    });
-  
-  
+  };
+
+
+  const filtering = (inputValue) => {
+    setBlock();
+    for (let i = 0; i < 12; i++) {
+      let storeItem = el[i].dataset.item;
+      if ((!storeItem.includes(inputValue))) {
+        el[i].style.display = "none";
+      }
+    }
+  };
+
+  const displayValue = (event) => {
+    if (event.keyCode === 13) {
+      setBlock();
+      let myInput = inputBar.value.toLowerCase();
+      event.preventDefault();
+      filtering(myInput);
+    }
+
+  };
+
+  const makeHandler = (param) => {
+    return function (event) {
+      event.preventDefault();
+      filtering(param);
+    };
+  };
+
+
+
+  for (let i = 1; i < filterButtons.length; i++) {
+    // gets all the items i.e cakes, doughnuts... and sets them to handlerParam per loop
+    let handlerParam = filterButtons[i].dataset.filter;
+    filterButtons[i].addEventListener("click", makeHandler(handlerParam));
+  };
+
+  filterButtons[0].addEventListener("click", setBlock);
+  inputBar.addEventListener("keyup", displayValue);
+
 })();
 
+// filterButton end
 
-/*
-Notes during project
-1. Tried to add an event listener to a node list. Had to actually do a forEach on the node list.
-https://stackoverflow.com/questions/55667793/uncaught-typeerror-input-addeventlistener-is-not-a-function
-2. Tried to use style.classList and then .show instead of just the string show. 'style not needed'
-3. How to access the individual CSS properties (without simply adding a class)
-4. I struggled with change the background. I thought I would be able to simply use element.style.background = 'image' but I realized I needed to use 'url(img);
-5. Had a bug in my code where I misspelled background. backgound vs. background 
+// Get all the images on the page and add them to an array.
+const pics = document.getElementsByClassName("card-img-top");
+let imageList = []
+for (let i = 0; i < pics.length; i++) {
+  const itemPicture = pics[i].src
+  imageList.push(itemPicture);
+}
 
-*/
+
+// for (let i = 0; i < pics.length; i++) {
+//   pics[i].onclick = () => {
+//     const toShow = document.querySelector(".lightbox-container");
+//     const showItem = document.querySelector(".lightbox-item");
+//     const closeButton = document.querySelector(".lightbox-close");
+//     const previousButton = document.querySelector(".btnLeft")
+//     const nextButton = document.querySelector(".btnRight")
+//     toShow.classList.add("show");
+
+//     let currentItem = pics[i]; // get the img element of the current iteration
+//     // console.log(currentItem)
+//     //gets the index of the currrent background image from the imageList array.
+//     let currentIndex = imageList.indexOf(currentItem.src);
+//     showItem.style.backgroundImage = `url(${currentItem.src})`;
+
+//     const previousImage = () => {
+//       if (currentIndex > 0) {
+//         currentIndex--;
+//       } else {
+//         currentIndex = 0;
+//       }
+//       let imageInImageList = imageList[currentIndex];
+//       console.log(imageInImageList);
+//       showItem.style.backgroundImage = `url(${imageInImageList})`
+//     };
+
+//     const nextImage = () => {
+//       if (currentIndex < imageList.length - 1) {
+//         currentIndex++;
+//       } else {
+//         currentIndex = imageList.length - 1;
+//       }
+//       let imageInImageList = imageList[currentIndex];
+//       console.log(imageInImageList);
+//       showItem.style.backgroundImage = `url(${imageInImageList})`
+//     };
+
+//     const closeModal = () => {
+//       toShow.classList.remove("show");
+//     };
+
+//     previousButton.addEventListener("click", previousImage);
+//     nextButton.addEventListener("click", nextImage);
+//     closeButton.addEventListener("click", closeModal);
+//   }
+// }
+
+const myModal = (event) => {
+  const toShow = document.querySelector(".lightbox-container");
+  const showItem = document.querySelector(".lightbox-item");
+  const closeButton = document.querySelector(".lightbox-close");
+  const previousButton = document.querySelector(".btnLeft")
+  const nextButton = document.querySelector(".btnRight")
+  toShow.classList.add("show");
+
+  let currentItem = event.target;// get the img element of the current iteration
+  //gets the index of the currrent background image from the imageList array.
+  let currentIndex = imageList.indexOf(currentItem.src);
+  showItem.style.backgroundImage = `url(${currentItem.src})`;
+
+
+  const previousImage = () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+    } else {
+      currentIndex = 0;
+    }
+    let imageInImageList = imageList[currentIndex];
+    showItem.style.backgroundImage = `url(${imageInImageList})`
+  };
+
+  const nextImage = () => {
+    if (currentIndex < imageList.length - 1) {
+      currentIndex++;
+    } else {
+      currentIndex = imageList.length - 1;
+    }
+    let imageInImageList = imageList[currentIndex];
+    showItem.style.backgroundImage = `url(${imageInImageList})`
+  };
+
+  const closeModal = () => {
+    toShow.classList.remove("show");
+  };
+
+  previousButton.addEventListener("click", previousImage);
+  nextButton.addEventListener("click", nextImage);
+  closeButton.addEventListener("click", closeModal);
+
+};
+
+for (let i = 0; i < pics.length; i++) {
+  pics[i].addEventListener("click", myModal);
+}
